1e0ddbeee26fae29133a234b5402e00c118731c1
Nicole Dominguez <sodevious.net@gmail.com>
2014-01-29 12:12:24 -0500

    Initialize with current files from sawhorsemedia.com

---
+++ index_old.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Sawhorse Media</title>
+<link rel="stylesheet" type="text/css" media="all" href="/style.css?1" />
+</head>
+<body>
+<div class="main container_16">
+	<h1>
+		Sawhorse Media
+	</h1>
+	<h2>
+		Our sites
+	</h2>
+	<ul>
+		<li><a href="http://www.musebin.com"><h3>Musebin <br/><span class="description">1-line music reviews</span></h3></a><p>Read about it on Wired.com [<a href="http://blog.wired.com/music/2008/11/musebins-twitte.html">1</a>] [<a href="http://blog.wired.com/business/2009/01/musebins-140-ch.html">2</a>], <a href="http://latimesblogs.latimes.com/music_blog/2009/01/musebin-the-fut.html">L.A. Times</a>, <a href="http://www.thrillist.com/new-york/musebin">Thrillist</a>, and <a href="http://www.ft.com/cms/s/0/b58520a6-d6a2-11dd-9bf7-000077b07658.html?nclick_check=1">Financial Times</a>.<br/>See the Musebin API in use on <a href="http://hypem.com/zeitgeist/2008/albums">Hype Machine</a>.</p></li>
+		<li><a href="http://www.shortyawards.com"><h3>Shorty Awards <br/><span class="description">The best producers of short content on Twitter</span></h3></a><p>Read about it on <a href="http://bits.blogs.nytimes.com/2008/12/15/from-the-twitterspere-tropies-for-the-best-tweets/">NYTimes.com</a>, <a href="http://www.techcrunch.com/2008/12/10/the-shorty-awards-honor-twitters-elite-tweets/">TechCrunch</a>, <a href="http://venturebeat.com/2008/12/11/the-shorty-awards-like-the-oscars-but-for-twitter/">VentureBeat</a>, and <a href="http://www.mediabistro.com/fishbowlny/awards/the_shorty_awards_its_not_how_long_it_is_its_how_you_phrase_it_103521.asp">mediabistro.com</a>.</p></li>
+		<li><a href="http://www.newsgroper.com"><h3>News Groper <br/><span class="description">Fake blogs</span></h3></a></li>
+	</ul>
+	<h2>
+		Our toys
+	</h2>
+	<ul>
+		<li><a href="/flickr-my-background"><h3>Flickr my background <br/><span class="description">Put your latest Flickr image in your Twitter background</span></h3></a></li>
+	</ul>
+	<h2>
+		Contact
+	</h2>
+	<ul>
+		<li><a href="http://twitter.com/sawhorse"><h3><span class="description">Twitter</span> @sawhorse</h3></a></li>
+		<li><a href="mailto:hello@sawhorsemedia.com"><h3><span class="description">Email</span> </span>hello@sawhorsemedia.com</h3></a></li>
+	</ul>
+	<div class="clear">&nbsp;</div>
+</div>
+</body>
+</html>
