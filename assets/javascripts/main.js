c1fc3cca91793953c7bc05bc7631a6479143ab19
Nicole Dominguez <sodevious.net@gmail.com>
2014-02-06 15:09:37 -0500

    scrolling nav bar

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,5 +1,10 @@
 $(function () {
-
     $('.carousel').carousel({interval: 8000});
 
+	$(document).scroll(function() {    
+		var scroll = $(this).scrollTop();
+		if (scroll >= 350) {
+			$("nav").addClass("on-scroll");
+		}
+	});
 });
